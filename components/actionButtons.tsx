3664496420c3527d0abdd6f5058c91153464adc8
PraWater <praneel.maddula@gmail.com>
2024-09-14 18:47:54 +0530

    add: ActionButtons

---
+++ components/actionButtons.tsx
@@ -0,0 +1,51 @@
+import { StyleSheet, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function ActionButtons() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonsContainer}>
+        <Ionicons name="pencil" size={30} color="#F3F3F3" />
+        <Ionicons name="image" size={30} color="#F3F3F3" />
+      </View>
+      <View style={styles.cameraContainer}>
+        <Ionicons name="camera" size={50} color="#090909" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    bottom: 40,
+    position: "absolute",
+    width: 210,
+    height: 84,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonsContainer: {
+    backgroundColor: "#090909",
+    width: 210,
+    height: 60,
+    borderRadius: 12,
+    borderColor: "#96BAC9",
+    borderWidth: 2,
+    borderStyle: "solid",
+    position: "absolute",
+    flex: 1,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: 14,
+    paddingRight: 14
+  },
+  cameraContainer: {
+    backgroundColor: "#96BAC9",
+    height: 84,
+    width: 84,
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
