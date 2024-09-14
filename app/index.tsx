b6b53affc8e20a004d89b5f72864a6fb4a959e87
radnus321 <f20220165@pilani.bits-pilani.ac.in>
2024-09-14 19:08:58 +0530

    merge: ActionButton and Header

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,24 @@
 import { View, StyleSheet } from "react-native";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import ActionButtons from "@/components/actionButtons";
 export default function Index() {
   return (
     <View style = {styles.container}>
     <Navbar/>
     <Hero/>
+    <ActionButtons/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
     backgroundColor: "#090910",
     height: "100%",
     width: "100%",
     padding: 0,
+    alignItems: "center",
     margin: 0,
   }
 })
